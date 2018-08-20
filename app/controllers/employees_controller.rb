57dd5ed3288475bc17bb13956c4d79fc93832687
github username <github email address>
2018-08-20 18:12:57 +0100

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,46 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    if @employee.valid?
+      @employee.save
+      redirect_to employee_path(@employee)
+    else
+      flash[:errors] = @employee.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    if @employee.update
+      redirect_to employee_path(@employee)
+    else
+      flash[:errors] = @employee.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+  end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+
+  validates :alias, uniqueness: true
+  validates :title, uniqueness: true
+
 end
